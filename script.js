aed9f9d8069cf4df2f6aadc2584e0e4b65055c1c
Marlon351 <marlonrodriguez351@gmail.com>
2023-08-01 15:49:05 -0400

    completedProject

---
+++ script.js
@@ -0,0 +1,7 @@
+let divP = document.getElementById("#p-div");
+let divImg = document.getElementById("#img-div");
+let divPButton = document.getElementById("#p-button");
+let divImgButton = document.getElementById("#img-button");
+divPButton.onmouseover = function(){
+    body.style.backgroundColor = "red";
+}
